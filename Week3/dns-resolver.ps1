fdb6b7039522524db3fa84463ea2bab260152413
Dylan <112408682+dylpall@users.noreply.github.com>
2026-01-29 10:15:37 -0500

    Add files via upload

---
+++ Week3/dns-resolver.ps1
@@ -0,0 +1,14 @@
+﻿param($network,$server)
+
+for ($prefix = 1; $prefix -le 254; $prefix++ ){
+
+    $ip="$network.$prefix"
+
+    $DNS=Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select-Object -ExpandProperty Namehost
+    
+    if ($DNS -ne $null){
+
+        echo "$ip $DNS"
+
+    }
+}
